75c3b2ed88ae2578c8901ddc1f34fffcb6281744
Ivan Macsemchuk <ivan.macsemchuk@gmail.com>
2022-11-07 18:26:32 -0600

    Main Menu, Background, Folder setup

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void Quit(){
+        Application.Quit();
+    }
+}
