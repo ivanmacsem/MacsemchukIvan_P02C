8ca0e00065f37c3b0141866aa33e5b8b9a2f56f8
Ivan Macsemchuk <ivan.macsemchuk@gmail.com>
2022-11-08 01:14:43 -0600

    State Machine Setup

---
+++ Assets/Scripts/StateMachine/CardGameSM.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardGameSM : MonoBehaviour
+{
+    void Start()
+    {
+        //starting State here
+    }
+}
---
+++ Assets/Scripts/StateMachine/CardGameState.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CardGameSM))]
+public class CardGameState : State
+{
+    protected CardGameSM StateMachine {get; private set;}
+
+    void Awake(){
+        StateMachine = GetComponent<CardGameSM>();
+    }
+}
