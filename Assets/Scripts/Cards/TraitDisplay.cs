c578c826cf7d8a7af171745be067aa374d9ca5f1
Ivan Macsemchuk <ivan.macsemchuk@gmail.com>
2022-11-12 23:57:52 -0600

    Trait display

--- Assets/Scripts/Cards/TraitDisplay.cs
+++ Assets/Scripts/Cards/TraitDisplay.cs
@@ -9,21 +9,12 @@ public class TraitDisplay : MonoBehaviour
 
     private Image icon;
     public Text traitTxt;
-    public List<Sprite> traitIcons = new List<Sprite>();
-    private string[] traitList = new string[] {"Rush", "Shield", "Spray", "Banish", "Callback", "Ranged", "Double Strike", "Taunt"};
 
     void Awake(){
         icon = GetComponent<Image>();
     }
     void OnEnable(){
-        int idx = -1;
-        for(int i = 0; i<traitList.Length; i++){
-            if(trait.name == traitList[i]){
-                idx = i;
-                break;
-            }
-        }
-        icon.sprite = traitIcons[idx];
+        icon.sprite = trait.icon;
         icon.gameObject.SetActive(true);
         traitTxt.text = trait.name;
     }
